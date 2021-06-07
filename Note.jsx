3e7479afd2b8c4a3cf6196476a087663a6913cb3
Prajakta Suryavanshi <61220786+Prajakta5999@users.noreply.github.com>
2021-06-07 15:34:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        React is a JavaScript library for building user interfaces. React is
+        used to build single page applications. React allows us to create
+        reusable UI components.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
